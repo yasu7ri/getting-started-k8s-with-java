286744d9a4b1e49b9e0b96ca157dd214b2040f42
yasu7ri <yasu7ri@hotmail.com>
2018-01-25 17:31:40 +0900

    no message

--- src/main/java/com/example/AppController.java
+++ src/main/java/com/example/AppController.java
@@ -1,4 +1,14 @@
 package com.example;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AppController {
+    @RequestMapping("/version")
+    public VersionResponse getVersion(){
+        VersionResponse response = new VersionResponse();
+        response.setVersion("1.0.0");
+        return response;
+    }
 }
--- src/main/java/com/example/VersionResponse.java
+++ src/main/java/com/example/VersionResponse.java
@@ -1,4 +1,13 @@
 package com.example;
 
 public class VersionResponse {
+    private String version;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
 }
