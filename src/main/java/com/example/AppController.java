c39b704c83db724686b7ee424de9227e32edc13a
YASUNARI TANAKA <yasu7ri@hotmail.com>
2018-02-11 21:08:15 +0900

    no message

--- src/main/java/com/example/AppController.java
+++ src/main/java/com/example/AppController.java
@@ -8,7 +8,7 @@ public class AppController {
     @RequestMapping("/version")
     public VersionResponse getVersion(){
         VersionResponse response = new VersionResponse();
-        response.setVersion("1.0.0");
+        response.setVersion("2.0.0");
         return response;
     }
 }
